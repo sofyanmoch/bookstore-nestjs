05482e6b84432d02bfa472581540014b6b70fd93
sofyanmoch <mchsfyan17@gmail.com>
2020-12-04 16:50:07 +0700

    add crud

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -3,7 +3,6 @@ import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body,
 import { BlogService } from './blog.service'
 import { CreatePostDTO } from './dto/create-post.dto'
 import { ValidateObjectId } from './shared/pipes/validate-object-id.pipes'
-import { POINT_CONVERSION_HYBRID } from 'constants';
 
 
 @Controller('blog')
@@ -11,20 +10,20 @@ export class BlogController {
 
     constructor(private blogService: BlogService) { }
 
-    @Get('posts')
+    @Get('/posts')
     async getPosts(@Res() res) {
         const posts = await this.blogService.getPosts()
         return res.status(HttpStatus.OK).json(posts)
     }
 
-    @Get('post/:postID')
+    @Get('/posts/:postID')
     async getPost(@Res() res, @Param('postID', new ValidateObjectId()) postID) {
         const post = await this.blogService.getPost(postID)
         if(!post) throw new NotFoundException('post does not exist!')
         return res.status(HttpStatus.OK).json(post)
     }
 
-    @Post('/post')
+    @Post('/posts/add')
     async addPost(@Res() res, @Body() createPostDTO: CreatePostDTO) {
         const newPost = await this.blogService.addPost(createPostDTO)
         return res.status(HttpStatus.OK).json({
@@ -33,10 +32,10 @@ export class BlogController {
         })
     }
 
-    @Put('/edit')
+    @Put('/edit/:postID')
     async editPost(
         @Res() res,
-        @Query('postID', new ValidateObjectId()) postID,
+        @Param('postID', new ValidateObjectId()) postID,
         @Body() createPostDTO: CreatePostDTO
     ) {
         const editedPost = await this.blogService.editPost(postID, createPostDTO)
