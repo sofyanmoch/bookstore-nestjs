3afd86eddefa80845a8e69d5ce76dca1f3a98c96
sofyanmoch <mchsfyan17@gmail.com>
2020-12-04 15:12:38 +0700

    add update and delete

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body, Query, Put, Delete } from '@nestjs/common';
 import { BlogService } from './blog.service'
 import { CreatePostDTO } from './dto/create-post.dto'
 import { ValidateObjectId } from '../shared/pipes/validate-object-id.pipes'
+import { POINT_CONVERSION_HYBRID } from 'constants';
 
 
 @Controller('blog')
@@ -23,11 +25,35 @@ export class BlogController {
     }
 
     @Post('/post')
-    async addPost(@Res() resizeBy, @Body() createPostDTO: CreatePostDTO) {
+    async addPost(@Res() res, @Body() createPostDTO: CreatePostDTO) {
         const newPost = await this.blogService.addPost(createPostDTO)
         return res.status(HttpStatus.OK).json({
             message: "Post has been submitted succesfully",
             post: newPost
+        })
+    }
+
+    @Put('/edit')
+    async editPost(
+        @Res() res,
+        @Query('postID', new ValidateObjectId()) POINT_CONVERSION_HYBRID,
+        @Body() createPostDTO: CreatePostDTO
+    ) {
+        const editedPost = await this.blogService.editPost(postID, createPostDTO)
+        if(!editedPost) throw new NotFoundException('Post does not exist!')
+        return res.status(HttpStatus.OK).json({
+            message: 'Post has been succesfully updated',
+            post: editedPost
+        })
+    }
+
+    @Delete('/delete')
+    async deletePost(@Res() res, @Query('postID', new ValidateObjectId()) postID) {
+        const deletedPost = await this.blogService.deletePost(postID)
+        if(!deletedPost) throw NotFoundException('Post does not exist!')
+        return res.status(HttpStatus.OK).json({
+            message: 'Post has been deleted!',
+            post: deletedPost
         })
     }
 }
